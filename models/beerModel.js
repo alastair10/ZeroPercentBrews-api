e51570fab32cd6968741acf9df0ccd2802a91097
Alastair <alastair.chau@gmail.com>
2023-01-04 22:00:49 +0000

    Refactor schema and add seed data files (WIP)

--- models/beerModel.js
+++ models/beerModel.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const beerSchema = new Schema({
   title: { type: String, required: true },
   brand: { type: String, required: true },
   abv: { type: Number, required: true },
   calories: { type: Number, required: true },
-  style: { type: String, required: true },
+  type: { type: String, required: true },
   ingredients: { type: String, required: true },
   country: { type: String, required: true },
-  vol: { type: Number, required: true },
-  overall: { type: Number, required: true },
-  taste: { type: Number, required: true },
-  realness: { type: Number, required: true },
-  value: { type: Number, required: true },
-  reviewCount: { type: Number, required: true },
+  volume: { type: Number, required: true },
+  overall: { type: Number, required: false },
+  taste: { type: Number, required: false },
+  realness: { type: Number, required: false },
+  value: { type: Number, required: false },
+  reviewCount: { type: Number, required: false },
 });
 
 // create the Beer model
