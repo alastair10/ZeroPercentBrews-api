1aa837adba3111638f9be8618bc335f5d5aed49e
Alastair <alastair.chau@gmail.com>
2023-01-04 16:51:48 +0000

    Add middleware to console log requests

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome to the app'})
-})
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
