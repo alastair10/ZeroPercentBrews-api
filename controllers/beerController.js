191ec31e541d8da30e00dfee64a98973d1b3293a
adonl <adnlwly@gmail.com>
2023-01-05 11:20:46 +0000

    require beerModel in controller

--- controllers/beerController.js
+++ controllers/beerController.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const Beer = require('../models/beerModel')
 
 const getBeers = async (req, res) => {
   
-  const allBeers = await Workout.find({});
+  const allBeers = await Beer.find({});
 
   res.status(200).json(allBeers);
 }
