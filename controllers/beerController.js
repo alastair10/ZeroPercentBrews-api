a5f88d332c3a1864c201a6fe66364cdf2f06ffd1
adonl <adnlwly@gmail.com>
2023-01-05 21:44:24 +0000

    update multiple CORs

--- controllers/beerController.js
+++ controllers/beerController.js
@@ -1,19 +1,34 @@
 const mongoose = require('mongoose');
 const Beer = require('../models/beerModel')
 
+
+
 const getBeers = async (req, res) => {
+  const allowedOrigins = ['http://localhost:3000', 'https://zero-percent-brews-client.vercel.app/'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
   
   const allBeers = await Beer.find({});
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000' || 'https://zero-percent-brews-client.vercel.app/');
   res.status(200).json(allBeers);
 }
 
 const getBeerById = async (req, res) => {
+  const allowedOrigins = ['http://localhost:3000', 'https://zero-percent-brews-client.vercel.app/'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
 
   const { id } = req.params;
-
   const beerItem = await Beer.findById(id);
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000' || 'https://zero-percent-brews-client.vercel.app/');
   res.status(200).json(beerItem);
 }
 
