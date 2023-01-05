f3b03860507cec84360b824a1355fe6dd17d92c4
adonl <adnlwly@gmail.com>
2023-01-05 11:39:52 +0000

    remove temp routes

--- routes/beers.js
+++ routes/beers.js
@@ -12,20 +12,5 @@ router.get("/", getBeers);
 // get one
 router.get("/:id", getBeerById);
 
-// post
-router.post("/", (req, res) => {
-  res.json({ message: "POST a new beer" });
-});
-
-// delete
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE a beer" });
-});
-
-// update
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE a beer" });
-});
-
 module.exports = router;
  
