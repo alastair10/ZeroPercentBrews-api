4c4c8b1f3d5eb548642e307ae43fd3702ffb3dc6
adonl <adnlwly@gmail.com>
2023-01-05 16:15:24 +0000

    update seed data

--- seeds/seed_beerData.js
+++ seeds/seed_beerData.js
@@ -4,62 +4,164 @@ const data = [
     brand: 'Adnams',
     abv: '0.5',
     calories: '23',
-    type: 'Beer',
+    type: 'Ale',
     ingredients: 'Water, Malted Barley, Malted Rye, Hops, Yeast',
     country: 'UK',
     volume: '500',
+    description: 'An un-boo-lievably crisp taste. Will raise your spirits if shipwrecked.',
+    reviews: [
+      {
+        author: "alastair",
+        body: "Makes me smile!"
+      },
+      {
+        author: "joe",
+        body: "Is this vegan? Unsure..."
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 5,
+      value: 3,
+      realness: 5
+    }
   },
   {
     title: 'Bavaria 0.0% Beer',
     brand: 'Bavaria',
     abv: '0',
     calories: '24',
-    type: 'Beer',
+    type: 'Lager',
     ingredients:
       'Natural Mineral Water, Barley Malt, Glucose Syrup, Hop Extract, Flavouring',
     country: 'Netherlands',
     volume: '330',
+    description: 'Definitely not the Wurst. You do not need a Wiesn to try this.',
+    reviews: [
+      {
+        author: "adon",
+        body: "Light and refreshing. Just my cuppa beer."
+      },
+      {
+        author: "Tom",
+        body: "Goes well with a cheese and pickle sandwich. No mayo."
+      }
+    ],
+    rating: {
+      overall: 3,
+      taste: 4,
+      value: 3,
+      realness: 3
+    }
   },
   {
     title: 'Becks Blue Alcohol Free',
     brand: 'Becks',
     abv: '0.05',
     calories: '14',
-    type: 'Beer',
+    type: 'Lager',
     ingredients: 'Water, Barley Malt, Hops, Yeast',
     country: 'Germany',
     volume: '330',
+    description: 'You don\'t have to be Posh to love a Becks.',
+    reviews: [
+      {
+        author: "Alastair",
+        body: "I drink this on the way home from my corporate job."
+      },
+      {
+        author: "Tom",
+        body: "Too mainstream for my liking. They were better when independently owned."
+      }
+    ],
+    rating: {
+      overall: 3,
+      taste: 2,
+      value: 3,
+      realness: 2
+    }
   },
   {
     title: 'Big Drop Brewing Paradiso Citra Ipa',
     brand: 'Big Drop Brewing Co.',
     abv: '0.5',
     calories: '18.5',
-    type: 'Beer',
+    type: 'Ale',
     ingredients: 'Water, Barley, Oats, Wheat, Lactose (Milk) Hops, Yeast',
     country: 'UK',
     volume: '330',
+    description: 'Citrusy undertones. Crisp taste. Will not leave you feeling bitter.',
+    reviews: [
+      {
+        author: "Adon",
+        body: "Not usually a fan of fruity beers but this is okay."
+      },
+      {
+        author: "Joe",
+        body: "Big Drop? More like Big Flop."
+      }
+    ],
+    rating: {
+      overall: 2,
+      taste: 2,
+      value: 1,
+      realness: 4
+    }
   },
   {
     title: 'Birra Moretti Zero',
     brand: 'Birra Moretti',
     abv: '0',
     calories: '20',
-    type: 'Beer',
+    type: 'Lager',
     ingredients: 'Water, Barley Malt, Hop, Hop Extract, Natural Flavouring',
     country: 'Italy',
     volume: '330',
+    description: 'Quality beer, authentic Italian taste.',
+    reviews: [
+      {
+        author: "Tom",
+        body: "Love me a Moretti I do. Glad they do a 0% one now"
+      },
+      {
+        author: "Joe",
+        body: "Brilliant beer. No complaints. I promise."
+      }
+    ],
+    rating: {
+      overall: 5,
+      taste: 5,
+      value: 4,
+      realness: 5
+    }
   },
   {
     title: 'Brewdog Punk Af',
     brand: 'Brewdog',
     abv: '0.5',
     calories: '15',
-    type: 'Beer',
+    type: 'Ale',
     ingredients:
       'Water, Lactose (Milk), Malted Barley, Hops, Yeast, Malted Oats, Malted Wheat, Lactic Acid',
     country: 'UK',
     volume: '330',
+    description: 'For the mainstream beer rebels who love to conform against the grain.',
+    reviews: [
+      {
+        author: "Adon",
+        body: "This one is a little strong for me. I felt tipsy after one."
+      },
+      {
+        author: "Alastair",
+        body: "Nice and chill. Like me."
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 4,
+      value: 4,
+      realness: 5
+    }
   },
   {
     title: 'Brooklyn Alcohol Free Hoppy Lager',
@@ -70,6 +172,23 @@ const data = [
     ingredients: 'Water, Malted Barley, Hops',
     country: 'EU',
     volume: '330',
+    description: 'Pale caramel notes, with a piney finish.',
+    reviews: [
+      {
+        author: "Alastair",
+        body: "I'm American and I don't even know if this is from Brooklyn?!"
+      },
+      {
+        author: "Joe",
+        body: "This is quite nice."
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 4,
+      value: 3,
+      realness: 4
+    }
   },
   {
     title: 'Corona Cero Alcohol Free',
@@ -80,39 +199,107 @@ const data = [
     ingredients: 'Water, Barley Malt, Maize, Sugar, Hops, Natural Flavours',
     country: 'Belgium',
     volume: '330',
+    description: 'Quench your cerveza cravings with a crip Corona Cero.',
+    reviews: [
+      {
+        author: "Tom",
+        body: "Perfect for a warm summer sesh."
+      },
+      {
+        author: "Adon",
+        body: "Light and refreshing. Lovely."
+      }
+    ],
+    rating: {
+      overall: 5,
+      taste: 5,
+      value: 5,
+      realness: 5
+    }
   },
   {
     title: 'Doom Bar Zero Amber Ale',
     brand: 'Doom Bar',
     abv: '0',
     calories: '13',
-    type: 'Beer',
+    type: 'Ale',
     ingredients:
       'Water, Malted Barley, Sugar, Oats, Hops and Hop Extracts, Antioxidant: Ascorbic Acid, Natural Flavours',
     country: 'UK',
     volume: '500',
+    description: 'Perfectly balanced beer with a subtle taste',
+    reviews: [
+      {
+        author: "Joe",
+        body: "Shocking. Never again"
+      },
+      {
+        author: "alastair",
+        body: "I am a nice person but can't think of something positive to say."
+      }
+    ],
+    rating: {
+      overall: 2,
+      taste: 2,
+      value: 2,
+      realness: 2
+    }
   },
   {
     title: 'Erdinger Alkoholfrei Grapefruit',
     brand: 'Erdinger',
     abv: '0.3',
     calories: '25',
-    type: 'Beer',
+    type: 'Wheat beer',
     ingredients:
       '50% Erdinger Alkoholfrei (Water, Wheat Malt, Barley Malt, Hops, Yeast, Carbon Dioxide), 50% Refreshment Drink with Grapefruit',
     country: 'Germany',
     volume: '330',
+    description: 'Freshingly good after a light jog on a warm summer\'s day.',
+    reviews: [
+      {
+        author: "Tom",
+        body: "A New Gen Beer."
+      },
+      {
+        author: "Adon",
+        body: "Once tried it after Berlin half marathon, it was nice but also free..."
+      }
+    ],
+    rating: {
+      overall: 3,
+      taste: 3,
+      value: 3,
+      realness: 3
+    }
   },
   {
     title: 'Free Damm Lager',
     brand: 'Free Damm',
     abv: '0',
     calories: '20',
-    type: 'Beer',
+    type: 'Lager',
     ingredients:
       'Water, Barley Malt (8%), Maize, Glucose and Fructose Syrup, Rice, Hops',
     country: 'Spain',
     volume: '330',
+        description: 'Brewed in Barcelona with top quality ingredients.',
+    reviews: [
+      {
+        author: "Adon",
+        body: "Perfect on a beach in Barcelona"
+      },
+      {
+        author: "Joe",
+        body: "This one is alright!"
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 4,
+      value: 5,
+      realness: 4
+    }
   },
   {
     title: 'Heineken 0.0 Alcohol Free Lager',
@@ -123,6 +310,23 @@ const data = [
     ingredients: 'Water, Malted Barley, Hop Extract, Natural Flavourings',
     country: 'Scotland',
     volume: '330',
+        description: 'Fruity and refreshing malt notes. Incredibly light and refreshing.',
+    reviews: [
+      {
+        author: "alastair",
+        body: "it is definitely better than they used to make it years ago"
+      },
+      {
+        author: "Tom",
+        body: "Fruiiiiiiiitty and freeeeeeshhh."
+      }
+    ],
+    rating: {
+      overall: 3,
+      taste: 3,
+      value: 3,
+      realness: 4
+    }
   },
   {
     title: 'Kopparberg Alcohol Free Pear Cider',
@@ -134,6 +338,23 @@ const data = [
       'Carbonated Water, Fermented Pears, Pear Juice, Sugar, Acidifier (Citric Acid), Flavouring, Preservative (Potassium Sorbate), Antioxidant (E224/Sulphites)',
     country: 'Sweden',
     volume: '500',
+    description: 'Sweet and revitalising, refreshingly pear.',
+    reviews: [
+      {
+        author: "alastair",
+        body: "I drink at least two a day"
+      },
+      {
+        author: "Joe",
+        body: "I prefer the strawberry"
+      }
+    ],
+    rating: {
+      overall: 3,
+      taste: 5,
+      value: 3,
+      realness: 3
+    }
   },
   {
     title: 'Kopparberg Cider Plus Strawberry & Lime Alcohol Free',
@@ -145,6 +366,23 @@ const data = [
       'Carbonated Water, Fermented Apples, Juice (Apple, Strawberry, Elderberry & Lime), Sugar, Flavouring, Acid (Citric Acid), Preservative (Potassium Sorbate), Antioxidant (E224/Sulphites)',
     country: 'Sweden',
     volume: '500',
+    description: 'A delicious mix of raspberry and blackcurrant',
+    reviews: [
+      {
+        author: "Joe",
+        body: "Could do with more strawberry"
+      },
+      {
+        author: "Adon",
+        body: "Tastes like fizzy pop"
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 3,
+      value: 3,
+      realness: 3
+    }
   },
   {
     title: 'Kopparberg Mixed Fruit Alcohol Free',
@@ -156,6 +394,23 @@ const data = [
       'Carbonated Water, Fermented Apples, Juice (Apple, Blackcurrant, Elderberry, Raspberry), Sugar, Acidifier (Citric Acid), Flavouring, Preservative (Potassium Sorbate)',
     country: 'Sweden',
     volume: '500',
+    description: 'A delicious mix of raspberry and blackcurrant',
+    reviews: [
+      {
+        author: "Adon",
+        body: "Quite sweet but it's nice"
+      },
+      {
+        author: "alastair",
+        body: "Lovely lovely lovely"
+      }
+    ],
+    rating: {
+      overall: 3,
+      taste: 3,
+      value: 3,
+      realness: 3
+    }
   },
   {
     title: 'Leffe Blonde 0% Alcohol Free Beer',
@@ -167,6 +422,23 @@ const data = [
       'Water, Barley Malt, Maize, Barley, Sugar, Hops, Natural Aromas',
     country: 'Belgium',
     volume: '250',
+    description: 'Rich, fruity notes and full of character',
+    reviews: [
+      {
+        author: "Joe",
+        body: "Nice but you need it in the right glass"
+      },
+      {
+        author: "alastair",
+        body: "reminds me of Belgium"
+      }
+    ],
+    rating: {
+      overall: 5,
+      taste: 4,
+      value: 4,
+      realness: 5
+    }
   },
   {
     title: 'Lucky Saint 0.5%',
@@ -177,6 +449,23 @@ const data = [
     ingredients: 'Water, Barley, Yeast, Hops',
     country: 'Germany',
     volume: '330',
+    description: 'Not for the Saint-Hearted. Bavarianly biscuity taste.',
+    reviews: [
+      {
+        author: "Adon",
+        body: "This is one of my faves. Gets me a bit tipsy!"
+      },
+      {
+        author: "Joe",
+        body: "One of my go to brands now."
+      }
+    ],
+    rating: {
+      overall: 5,
+      taste: 5,
+      value: 4,
+      realness: 5
+    }
   },
   {
     title: 'Peroni Nastro Azzurro 0.0%',
@@ -187,6 +476,19 @@ const data = [
     ingredients: 'Water, Barley Malt, Italian Maize, Hops, Natural Flavours',
     country: 'Italy',
     volume: '330',
+    description: 'TEXT',
+    reviews: [
+      {
+        author: "alastair",
+        body: "i love this beer!!"
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 5,
+      value: 3,
+      realness: 5
+    }
   },
   {
     title: 'San Miguel 0.0%',
@@ -197,26 +499,73 @@ const data = [
     ingredients: 'Water, Barley Malt, Hops, Flavourings',
     country: 'Spain',
     volume: '330',
+    description: 'TEXT',
+    reviews: [
+      {
+        author: "alastair",
+        body: "i love this beer!!"
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 5,
+      value: 3,
+      realness: 5
+    }
   },
   {
     title: 'Shipyard Low Tide Pale Ale',
     brand: 'Shipyard',
     abv: '0.5',
     calories: '17',
-    type: 'Beer',
+    type: 'Ale',
     ingredients: 'Water, Barley, Wheat, Hops, Milk (Lactose), Yeast',
     country: 'UK',
     volume: '500',
+    description: 'The tide might be low but this will raise your spirits!',
+    reviews: [
+      {
+        author: "Joe",
+        body: "Great for a pale ale"
+      },
+      {
+        author: "alastair",
+        body: "niceeeee"
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 4,
+      value: 4,
+      realness: 5
+    }
   },
   {
     title: 'Stella Artois Premium Alcohol Free Lager',
     brand: 'Stella Artois',
     abv: '0',
     calories: '18',
-    type: 'Beer',
+    type: 'Lager',
     ingredients: 'Water, Malted Barley, Hops, Sugar, Natural Flavours',
     country: 'Belgium',
     volume: '330',
+    description: 'Hoppy, refreshing and no where',
+    reviews: [
+      {
+        author: "Tom",
+        body: "Nice nice nice"
+      },
+      {
+        author: "Adon",
+        body: "Not a huge fan"
+      }
+    ],
+    rating: {
+      overall: 3,
+      taste: 4,
+      value: 2,
+      realness: 4
+    }
   },
   {
     title: 'Thatchers Zero Somerset Cider',
@@ -227,6 +576,23 @@ const data = [
     ingredients: 'Cider apples',
     country: 'UK',
     volume: '500',
+    description: 'Medium-dry taste, bursting with appley goodness.',
+    reviews: [
+      {
+        author: "Adon",
+        body: "Lovely on a summer's evening!"
+      },
+      {
+        author: "Tom",
+        body: "Chillllllllll"
+      }
+    ],
+    rating: {
+      overall: 4,
+      taste: 3,
+      value: 4,
+      realness: 4
+    }
   },
 ];
 
