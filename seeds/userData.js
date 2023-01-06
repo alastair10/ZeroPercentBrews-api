e30267249e6c78bd435f67bf41320562b3f95c6a
Joseph Lander <josephclander@gmail.com>
2023-01-06 16:42:43 +0000

    Update user seed data

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,11 +1,30 @@
 // password is plain text and hashed by the model
 // favourites is blank as default is []
 // also ids change when seeded so will be incorrect
+
+// THIS IS DUMMY DATA
+
 const data = [
-  { email: 'joe@example.com', username: 'joe', password: '1234' },
-  { email: 'tom@example.com', username: 'tom', password: '1234' },
-  { email: 'alastair@example.com', username: 'alastair', password: '1234' },
-  { email: 'adon@example.com', username: 'adon', password: '1234' },
+  {
+    email: 'joe@example.com',
+    username: 'joe',
+    password: '$2b$10$0/V4mFlneXBWltlwbbsAkeTlZ2.FyKskw1qwiqFej6BZho8ur7nRm',
+  },
+  {
+    email: 'tom@example.com',
+    username: 'tom',
+    password: '$2b$10$KzP/cEQeYw8qiBoYsqlsgumnSa0l7o5xBBVeAdKLFszv2Sntx0ZJK',
+  },
+  {
+    email: 'alastair@example.com',
+    username: 'alastair',
+    password: '2b$10$HtRaoqrhATnV1OHKZVwHY.M1hc9WFitIRnxPqO/uWG9GiOeC8iUSC',
+  },
+  {
+    email: 'adon@example.com',
+    username: 'adon',
+    password: '2b$10$osOZ2jKN3ldzFS3/YQ0JNOr46nXLg/wV0VHTKzBaJE63ORp7MuElO',
+  },
 ];
 
 module.exports = data;
