2c83430812f94028b48a69ab05f9780376e86bed
Joseph Lander <josephclander@gmail.com>
2023-01-06 15:20:51 +0000

    Add seed_userData

---
+++ seeds/seed_userData.js
@@ -0,0 +1,11 @@
+// password is plain text and hashed by the model
+// favourites is blank as default is []
+// also ids change when seeded so will be incorrect
+const data = [
+  { email: 'joe@example.com', username: 'joe', password: '1234' },
+  { email: 'tom@example.com', username: 'tom', password: '1234' },
+  { email: 'alastair@example.com', username: 'alastair', password: '1234' },
+  { email: 'adon@example.com', username: 'adon', password: '1234' },
+];
+
+module.exports = data;
