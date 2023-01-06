b3f747cdf35ab4fd4c08fd28ac02be301edcb8dd
Adon Lawley <110059340+lwly-jpg@users.noreply.github.com>
2023-01-06 15:44:23 +0000

    update tests

--- tests/beerController.test.js
+++ tests/beerController.test.js
@@ -15,31 +15,6 @@ describe('/api/beers', () => {
         .get('/api/beers')
       expect(response.statusCode).toBe(200);
     });
-
-    it('returns success response for individual beer', async () => {
-      let response = await request(app)
-        .get('/api/beers/63b6fb1e77559bc7a95ffd3d')
-      expect(response.statusCode).toBe(200);
-    });
-  })
-
-  xit('returns beers from API', async () => {
-    let mockBeer = new Beer({
-      image: '',
-      title: 'Adnams Ghost Ship Bottle Beer 0.5%',
-      brand: 'Adnams',
-      abv: '0.5',
-      calories: '23',
-      type: 'Ale',
-      ingredients: 'Water, Malted Barley, Malted Rye, Hops, Yeast',
-      country: 'UK',
-      volume: '500',
-      description: 'An un-boo-lievably crisp taste. Will raise your spirits if shipwrecked.',
-    })
-    await mockBeer.save();
-    let response = await request(app)
-      .get("/posts");
-    expect(response.body).toEqual([]);
   })
 });
 
