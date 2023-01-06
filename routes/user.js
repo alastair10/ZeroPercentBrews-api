6709f4526d659f9bfc6d861ee1861e3a8b87b024
adonl <adnlwly@gmail.com>
2023-01-06 14:44:00 +0000

    create user routes

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const {
+  loginUser,
+  registerUser
+} = require('../controllers/userController');
+
+const router = express.Router();
+
+// register
+router.post("/register", registerUser);
+
+// log in
+router.post("/login", loginUser);
+
+module.exports = router;
