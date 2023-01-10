50eb9553b51e62046dbd19ca526d8ba5208703fe
adonl <adnlwly@gmail.com>
2023-01-10 10:08:31 +0000

    add get user route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
-const { loginUser, registerUser } = require('../controllers/userController');
+const { loginUser, registerUser, getUser } = require('../controllers/userController');
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
 
+// register
 router.post('/register', registerUser);
 
 // log in
 router.post('/login', loginUser);
+
+// get user
+router.get('/:id', requireAuth, getUser);
 
 module.exports = router;
